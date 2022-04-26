35662e4af4c4823236c8691c3b453de360111647
Conor Nolan <IAmConorNolan@gmail.com>
2022-04-26 12:50:39 +0100

    a

--- Sources/ZenBongo/ZenBongo.swift
+++ Sources/ZenBongo/ZenBongo.swift
@@ -1,6 +1,13 @@
 public struct ZenBongo {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public let bongoDevice: BongoInstrument
+
+    public init(bongoDevice: BongoInstrument) {
+        self.bongoDevice = bongoDevice
     }
+}
+
+public protocol BongoInstrument {
+    func bongo() -> String
 }
